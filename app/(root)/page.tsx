8fe38732aab78156394c42cfc5df245064219e67
Ibrahim Albadawi <140857058+ibrahimAlbadawi@users.noreply.github.com>
2025-11-12 21:45:49 +0300

    merges dev to main and fixed vercel compile errors

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,7 @@
 "use client";
 
-import { useEffect, useState } from "react";
 
 const Home = () => {
-  const [showLogo, setShowLogo] = useState(false);
-
-  useEffect(() => {
-    const id = requestAnimationFrame(() => setShowLogo(true));
-    return () => cancelAnimationFrame(id);
-  }, []);
-
-  useEffect(() => {
-    if (!showLogo) return;
-    const t = setTimeout(() => setShowText(true), 300); // show text after logo
-    return () => clearTimeout(t);
-  }, [showLogo]);
-
   return (
     <main className="min-h-screen  grid place-items-center px-4">
       <div className="w-full max-w-[320px] sm:max-w-[420px] md:max-w-[520px] text-center">
